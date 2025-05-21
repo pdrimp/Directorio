e6960efea8ef6940a21cb7dd31fcf9c903dbb444
Pedro Damián Reyes <damian@ucol.mx>
2025-05-20 19:57:22 -0600

    Agregar archivos de proyecto.

---
+++ Directorio/Data/Habito.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Directorio.Data
+{
+    public class Habito
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres")]
+        public string? Nombre { get; set; }
+    }
+}
---
+++ Directorio/Data/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Directorio.Data
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "El teléfono debe contener 10 dígitos")]
+        public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo no es válido")]
+        [StringLength(100, ErrorMessage = "El correo no puede exceder los 100 caracteres")]
+        public string? Correo { get; set; }
+    }
+}
